da7c6be7196396e0b516c7291f6064ca3a384245
sdwinter0 <sdwinter@wpi.edu>
2018-09-03 16:10:36 -0400

    Add files via upload

--- ConnectNth/src/Players/SimplePlayer1.java
+++ ConnectNth/src/Players/SimplePlayer1.java
@@ -21,31 +21,6 @@ public class SimplePlayer1 extends Player
 
     public Move getMove(StateTree state)
     {
-        for(int j=0; j<state.columns; j++)
-        {
-            for(int i=0; i<state.rows; i++)
-            {
-
-                if(state.getBoardMatrix()[i][j] == 0)
-                {
-                    return new Move(false, j);
-                }
-
-//				try{Thread.sleep(15000);}
-//				catch(InterruptedException ex){Thread.currentThread().interrupt();}
-
-//				if(this.turn == 1)
-//					return new Move(false, 0);
-//				if(this.turn == 2)
-//					return new Move(false, 1);
-            }
-
-//			if((this.turn == 1 && !state.pop1) || (this.turn == 2 && !state.pop2))
-//			{
-//				return new Move(true, 0);
-//			}
-
-        }
-        return new Move(false, 100);
+        return new Move(false, calculateHeuristic(state));
     }
 }
